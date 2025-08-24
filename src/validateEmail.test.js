410e14b183f658cd29b7bb7b9799df971ab2084e
Dmytro Koriahin <dkoryahin@gmail.com>
2025-08-24 19:49:36 +0300

    add task solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,7 +8,9 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const r = validateEmail('');
 
+    expect(typeof r).toBe('boolean');
   });
 
   it(`should return 'true' for the valid email`, () => {
@@ -16,5 +18,27 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
-  // write more tests here
+  it(`'false' if email has not '@'`, () => {
+    expect(validateEmail('test838gmail.com.')).toBe(false);
+  })
+
+  it(`'false' if email has cyrillic chars`, () => {
+    expect(validateEmail('тест838@gmail.com.')).toBe(false);
+  })
+
+  it(`'false' if email has double dot in 'personal_info'`, () => {
+    expect(validateEmail('test..838@gmail.com.')).toBe(false);
+  })
+
+  it(`'false' if 'personal_info' started with dot`, () => {
+    expect(validateEmail('.test838@gmail.com')).toBe(false);
+  })
+
+  it(`'false' if 'domain' didn't start with dot`, () => {
+    expect(validateEmail('test838@gmailcom')).toBe(false);
+  })
+
+  it(`'false' if email has not allowed chars`, () => {
+    expect(validateEmail(`test838!$%&'*+/=?^{|}~@gmailcom`)).toBe(false);
+  })
 });
