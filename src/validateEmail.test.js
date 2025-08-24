8bb6f3fb02301a018cef9b8b5ebd4549589de5be
Dmytro Koriahin <dkoryahin@gmail.com>
2025-08-24 19:57:41 +0300

    add task solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -18,27 +18,46 @@ describe(`Function 'validateEmail':`, () => {
       .toBeTruthy();
   });
 
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('test@mail.com'))
+      .toBeTruthy();
+  });
+
+
+  it(`should return 'true' for the valid email`, () => {
+    expect(validateEmail('t@q.c'))
+      .toBeTruthy();
+  });
+
   it(`'false' if email has not '@'`, () => {
     expect(validateEmail('test838gmail.com.')).toBe(false);
-  })
+  });
 
   it(`'false' if email has cyrillic chars`, () => {
     expect(validateEmail('тест838@gmail.com.')).toBe(false);
-  })
+  });
 
   it(`'false' if email has double dot in 'personal_info'`, () => {
     expect(validateEmail('test..838@gmail.com.')).toBe(false);
-  })
+  });
 
   it(`'false' if 'personal_info' started with dot`, () => {
     expect(validateEmail('.test838@gmail.com')).toBe(false);
-  })
+  });
 
-  it(`'false' if 'domain' didn't start with dot`, () => {
+  it(`'false' if 'domain' started with dot`, () => {
+    expect(validateEmail('test838@.gmailcom')).toBe(false);
+  });
+
+  it(`'false' if 'domain' hasn't any dot`, () => {
     expect(validateEmail('test838@gmailcom')).toBe(false);
-  })
+  });
 
   it(`'false' if email has not allowed chars`, () => {
     expect(validateEmail(`test838!$%&'*+/=?^{|}~@gmailcom`)).toBe(false);
-  })
+  });
+
+  it(`'false' if email has invalid 'domen'`, () => {
+    expect(validateEmail(`false@email`)).toBe(false);
+  });
 });
